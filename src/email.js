c62f42e8aeaab9ed2ea00424ee5c19f9a175ac4c
Abhinav Krishna C K <me@abhy.me>
2021-06-19 22:00:00 +0530

    Split inbound text content into multiple events
    if the size of the text content of inbound mail is greater than
    63k bytes, split the content into multiple events as the complete
    event shouldn't be greater than 65k.

--- src/email.js
+++ src/email.js
@@ -15,7 +15,6 @@ module.exports.startSMTP = new SMTPServer({
         let subject, text;
         const mailparser = new MailParser();
         const fromAdd = ParseEmailAddress.parseOneAddress(session.envelope.mailFrom.address);
-        // console.log(session.envelope.mailFrom.address)
 
         mailparser.on('headers', headers => {
             subject = headers.get('subject');
@@ -29,12 +28,25 @@ module.exports.startSMTP = new SMTPServer({
 
         mailparser.on('end', () => {
             if (!text.replace(/\s/g, '').length) {
-                // string only contains whitespace (ie. spaces, tabs or line breaks)
+                // text only contains whitespace (ie. spaces, tabs or line breaks)
                 return;
             }
             console.log(text);
+            // The complete event MUST NOT be larger than 65535 bytes.
+            // Using 63k as the maximum text size.
+            let message = Buffer.from(text, "utf-8");
             const intent = bridge.getIntent("@_email_" + fromAdd.local + "_" + fromAdd.domain + ":localhost");
-            intent.sendText(ROOM_ID, text);
+            if (message.byteLength > 63000) {
+                // split text to under 63k and send as seperate events.
+                console.log("Mail contents greater than 63k")
+                for (let i = 0; i<message.byteLength; i = i + 63000) {
+                    intent.sendText(ROOM_ID, message.toString("utf-8", i, i + 62999));
+                }
+            }
+            else {
+                // send message as a single event.
+                intent.sendText(ROOM_ID, text);
+            }
         });
 
         stream.pipe(mailparser);
